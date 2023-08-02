f6da3ee6b31bcd99e0ac055c14001fb9b918db98
Sailornina <sylcardozo.sc@gmail.com>
2023-08-02 15:25:54 +0200

    rename repo

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-study-group'`, () => {
+  it(`should have as title 'home-rentals'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-study-group');
+    expect(app.title).toEqual('home-rentals');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-study-group app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('home-rentals app is running!');
   });
 });
